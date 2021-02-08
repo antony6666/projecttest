8edc5bd4bec02407c3bb7b5355e070d5649e3d10
anthonymartin28 <77482894+anthonymartin28@users.noreply.github.com>
2021-02-08 14:55:02 +0100

    Update quizz.js

--- quizz.js
+++ quizz.js
@@ -1,24 +1,4 @@
-declare module "index.json" {
-  const value: any;
-  export default value;
-}
 
-
-function avancement() {
-  var ava = document.getElementById("avancement");
-  var prc = document.getElementById("pourcentage");
-  prc.innerHTML = "question numéro " + ava.value ;
-}
-
-avancement(); //Initialisation
-
-function modif(val) {
-  var ava = document.getElementById("avancement");
-  if((ava.value+val)<=ava.max && (ava.value+val)>0) {
-     ava.value += val;
-  }
-  avancement();
-}
 function avancement() {
   var ava = document.getElementById("avancement");
   var prc = document.getElementById("pourcentage");
